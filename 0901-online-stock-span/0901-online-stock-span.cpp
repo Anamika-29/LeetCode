111598726aff6e9b5e7ff55138a66a7e63c8b865
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-09 12:23:35 +0530

    Time: 356 ms (74.93%), Space: 84.1 MB (98.88%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,31 +1,34 @@
-class StockSpanner:
-
-    def __init__(self):
+class StockSpanner {
+public:
+    StockSpanner() {
+        // maintain a monotonic stack for stock entry
         
-        # maintain a monotonic stack for stock entry
+		// definition of stock entry:
+        // first parameter is price quote
+        // second parameter is price span
+    }
+    
+    int next(int price) {
         
-		## definition of stock entry:
-        # first parameter is price quote
-        # second parameter is price span
-        self.monotone_stack = []
-              
+        int curPrice = price;
+        int curSpan = 1;
         
-        
-    def next(self, price: int) -> int:
-
-        stack = self.monotone_stack
-        
-        cur_price_quote, cur_price_span = price, 1
-        
-        # Compute price span in stock data with monotonic stack
-        while stack and stack[-1][0] <= cur_price_quote:
+        // Compute price span in stock data with monotonic stack
+        while( stack.size() && stack.back().first <= price ){
             
-            prev_price_quote, prev_price_span = stack.pop()
+            auto [prevPrice, prevSpan] = stack.back();
             
-            # update current price span with history data in stack
-            cur_price_span += prev_price_span
+            stack.pop_back();
+            
+            // update current price span with history data in stack
+            curSpan += prevSpan;
+        }
         
-        # Update latest price quote and price span
-        stack.append( (cur_price_quote, cur_price_span) )
+        // update latest price quote and price span
+        stack.push_back( pair<int, int>{curPrice, curSpan} );
         
-        return cur_price_span
+        return curSpan;
+    }
+private:
+    vector< pair<int, int> > stack;
+};
