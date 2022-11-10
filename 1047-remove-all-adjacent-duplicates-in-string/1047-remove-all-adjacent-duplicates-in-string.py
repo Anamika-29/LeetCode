059e584767c2d4fdacfd8b5f1675042f22cac008
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-10 16:02:12 +0530

    Time: 158 ms (63.81%), Space: 14.8 MB (86.70%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -1,20 +1,9 @@
-class Solution {
-public:
-    string removeDuplicates(string s) {
-
-		string ans;
-		ans.push_back(s[0]);
-
-		for(int i=1 ; i<s.length() ; i++){
-
-			if(s[i] == ans.back()){
-				ans.pop_back();
-			}
-			else{
-				ans.push_back(s[i]);
-			}
-		}
-
-		return ans;
-	}
-};
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for ch in s:
+            if stack and ch == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(ch)
+        return "".join(stack)
