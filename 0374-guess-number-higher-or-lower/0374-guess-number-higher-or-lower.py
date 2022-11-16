cfd1751c8b221cbc742f694d789dd6f9561ada34
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-16 16:28:11 +0530

    Time: 44 ms (75.79%), Space: 13.9 MB (66.34%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -1,26 +1,22 @@
-/** 
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * int guess(int num);
- */
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
 
-class Solution {
-public:
-      int guessNumber(int n) {
-        int lo = 1, hi = n, mid, result;
-        
-        while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            result = guess(mid);
-            
-            if (result == 0) return mid;
-            if (result < 0) hi = mid - 1;
-            else lo = mid + 1;
-        }
-        
-        return lo;
-    }
-};
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        lowerBound, upperBound = 1, n
+        # Binary division faster than (lowerBound + upperBound) //2
+        myGuess = (lowerBound+upperBound) >> 1
+        # walrus operator ':=' - assigns value of the function to the variable 'res'
+        # and then compare res with 0
+        while (res := guess(myGuess)) != 0:
+            if res == 1:
+                lowerBound = myGuess+1
+            else:
+                upperBound = myGuess-1
+            myGuess = (lowerBound+upperBound) >> 1
+
+        return myGuess
