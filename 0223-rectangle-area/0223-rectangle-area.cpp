3643164c2bfa84de95ba7a1aaa69b1aa32aaa7dd
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-17 16:22:25 +0530

    Time: 24 ms (18.00%), Space: 5.8 MB (96.47%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,4 +1,14 @@
-class Solution:
-    def computeArea(self, A, B, C, D, E, F, G, H):
-        overlap = max(min(C,G)-max(A,E), 0)*max(min(D,H)-max(B,F), 0)
-        return (A-C)*(B-D) + (E-G)*(F-H) - overlap
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        
+        int areaOf1stRectangle = (ax2 - ax1) * (ay2 - ay1);
+        int areaOf2ndRectangle = (bx2 - bx1) * (by2 - by1);
+
+        int xOverlap = min(ax2, bx2) - max(ax1, bx1);
+        int yOverlap = min(ay2, by2) - max(ay1, by1);
+        int areaOverlap = (xOverlap > 0 && yOverlap > 0) ? xOverlap * yOverlap : 0;
+
+        return (areaOf1stRectangle + areaOf2ndRectangle - areaOverlap);    
+    }
+};
