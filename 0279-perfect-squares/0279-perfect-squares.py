b4094e0423cf44806f9a5bc5b854d79b2ac0fb86
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-22 17:36:37 +0530

    Time: 271 ms (81.10%), Space: 13.9 MB (88.62%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,18 +1,7 @@
-class Solution {
-public:
-   int numSquares(int n) {
-        //vector for updating the dp array/values
-        vector<int> dp(n+1,INT_MAX);
-        //base case
-        dp[0]=0;
-        int count = 1;
-        while(count*count <= n) {
-        int sq = count*count;
-        for(int i = sq; i < n+1; i++) {
-            dp[i] = min(dp[i-sq] + 1,dp[i]);
-        }
-        count++;
-    }
-    return dp[n];
-    }
-};
+class Solution(object):
+    _dp = [0]
+    def numSquares(self, n):
+        dp = self._dp
+        while len(dp) <= n:
+            dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
+        return dp[n]
