3ec091ba603c3989dbed479898963cd8347e0a45
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-24 17:12:36 +0530

    Time: 9775 ms (5.01%), Space: 14 MB (52.49%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,36 +1,30 @@
-class Solution {
-public:
-    bool DFS(vector<vector<char>>& board, string word, int i, int j, int n) {
-		//check if all the alphabets in the word is checked
-        if(n == word.size()) return true; 
-        
-		//check if i and j are out of bound or if the characters aren't equal
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n]) return false;
-        
-		//mark as visited 
-        board[i][j] = '0';
-        
-		//branch out in all 4 directions
-        bool status = DFS(board, word, i + 1, j, n + 1) ||  //down
-                        DFS(board, word, i, j + 1, n + 1) ||  //right
-                        DFS(board, word, i - 1, j, n + 1) ||  //up
-                        DFS(board, word, i, j - 1, n + 1);  //left
-        
-		//change the character back for other searches
-        board[i][j] = word[n];
-		
-        return status;
-    }
-    
-    bool exist(vector<vector<char>>& board, string word) {
-        if(word == "") return false;
-        
-        for(int i = 0; i < board.size(); i++) 
-            for(int j = 0; j < board[i].size(); j++) 
-				//check if the characters are equal then call DFS
-                if(board[i][j] == word[0] && DFS(board, word, i, j, 0))
-                    return true;
-        
-        return false;
-    }
-};
+class Solution:
+    def exist(self, board, word):
+        if not word:
+            return True
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.exist_helper(board, word, i, j):
+                    return True
+        return False
+
+    def exist_helper(self, board, word, i, j):
+        if board[i][j] == word[0]:
+            if not word[1:]:
+                return True
+            board[i][j] = " " # indicate used cell
+            # check all adjacent cells
+            if i > 0 and self.exist_helper(board, word[1:], i-1, j):
+                return True
+            if i < len(board)-1 and self.exist_helper(board, word[1:], i+1, j):
+                return True
+            if j > 0 and self.exist_helper(board, word[1:], i, j-1):
+                return True
+            if j < len(board[0])-1 and self.exist_helper(board, word[1:], i, j+1):
+                return True
+            board[i][j] = word[0] # update the cell to its original value
+            return False
+        else:
+            return False
