af3d78011950a29673013464075cf679e264ca02
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-26 17:19:49 +0530

    Time: 1402 ms (50.03%), Space: 27.4 MB (66.06%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,33 +1,9 @@
-class Solution {
-public:
-    vector<int>dp;
-int find(vector<vector<int>>& events,int idx,int end){
-	// We reach final index, return 0
-    if(idx==events.size())
-        return 0;
-	// If the current start time is less than previous end time then we can't attend this event, we move forward.
-    if( events[idx][0] < end )
-        return find( events, idx + 1 , end );
-	// Return pre stored value if we have 
-    if( dp[idx] != -1)
-        return dp[idx];
-    int res= max ( events[idx][2] + find(events, idx + 1 , events[idx][1] ),  find(events , idx + 1 , end ) );
-    return dp[idx] = res;
-}
-int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
-{
-    //putting startTime,endTimeand profit in an array so that we can sort it
-    vector<vector<int>>events(startTime.size(),vector<int>(3,0));
-    for(int i=0;i<startTime.size();i++)
-    {
-        events[i][0]=startTime[i];
-        events[i][1]=endTime[i];
-        events[i][2]=profit[i];
-    }
-    sort(events.begin() , events.end());
-    int n = events.size();
-	//Intialising DP and assigning value -1
-    dp.resize( n,-1);
-    return find(events , 0 , 0);
-}
-};
+class Solution:
+     def jobScheduling(self, startTime, endTime, profit):
+        jobs = sorted(zip(startTime, endTime, profit), key=lambda v: v[1])
+        dp = [[0, 0]]
+        for s, e, p in jobs:
+            i = bisect.bisect(dp, [s + 1]) - 1
+            if dp[i][1] + p > dp[-1][1]:
+                dp.append([e, dp[i][1] + p])
+        return dp[-1][1]
