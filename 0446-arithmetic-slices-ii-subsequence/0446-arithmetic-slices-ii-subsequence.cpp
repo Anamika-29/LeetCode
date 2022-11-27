3c01d388fe5057c930662d9229d089908117d0f0
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-27 13:05:12 +0530

    Time: 789 ms (85.49%), Space: 154.8 MB (51.65%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,10 +1,22 @@
-class Solution:
-    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        total, n = 0, len(nums)
-        dp = [defaultdict(int) for _ in nums]
-        for i in range(1, n):
-            for j in range(i):
-                diff = nums[j] - nums[i]
-                dp[i][diff] += dp[j][diff] + 1
-                total += dp[j][diff]
-        return total
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        vector< unordered_map<long, long> > dp(nums.size());
+        int res = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                long diff = (long)nums[i] - (long)nums[j];
+                
+                dp[i][diff]++;
+                
+                if (dp[j].find(diff) != dp[j].end()) {
+                    dp[i][diff] += dp[j][diff];
+                    res += dp[j][diff];
+                }
+            }
+        }
+        
+        return res;
+    }
+};
