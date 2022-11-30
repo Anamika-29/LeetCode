d6afc952b67d5db6f9f940cbe7d9772d121cb7ab
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-11-30 15:53:34 +0530

    Time: 73 ms (39.73%), Space: 14.2 MB (5.18%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,23 +1,14 @@
-class Solution {
-public:
-    bool uniqueOccurrences(vector<int>& arr) {
-        int i = 0;
-        sort(arr.begin(),arr.end());
-        vector<int> ans;
-        while (i < arr.size()){
-            int count = 1;
-            for (int j = i+1; j< arr.size(); j++){
-                if (arr[i] == arr[j])
-                    count++;
-            }
-            ans.push_back(count);
-            i = i + count;
-        }
-        sort(ans.begin(),ans.end());
-        for (int i = 0; i < ans.size() - 1; i++){
-            if (ans[i] == ans [i+1])
-                return false;
-        }
-        return true;
-    }
-};
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        dic = {}
+        
+        # create a hash map
+        for i in arr:
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+        
+        if len(set(dic.values())) != len(set(arr)):
+            return False
+        return True   
