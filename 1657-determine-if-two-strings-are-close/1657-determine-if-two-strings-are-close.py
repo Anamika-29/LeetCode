4c2868c847818a7bc22352f70942e74cb1b95115
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-02 16:37:32 +0530

    Time: 633 ms (23.60%), Space: 16.7 MB (5.20%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,27 +1,19 @@
-class Solution {
-public:
-    bool closeStrings(string word1, string word2) {
-        int len=word1.length();
-        if(word2.length()!=len)
-            return false;
-        
-        vector<int> count1(26,0);
-        vector<int> count2(26,0);
-        for(int i=0;i<len;i++){
-            count1[word1[i]-'a']++;
-            count2[word2[i]-'a']++;
-        }
-        //Checking if same characters exist
-        for(int i=0;i<26;i++){
-            if((count1[i]>0 && count2[i]==0) || (count1[i]==0 && count2[i]>0))
-                return false;
-        }
-        sort(begin(count1),end(count1));
-        sort(begin(count2),end(count2));
-        for(int i=0;i<26;i++){
-            if(count1[i]!=count2[i])
-                return false;
-        }
-        return true;
-    }
-};
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        w1=list(word1)
+        w2=list(word2)
+        d1=defaultdict(lambda:0)
+        d2=defaultdict(lambda:0)
+        for a in w1:
+            d1[a]+=1
+        for a in w2:
+            d2[a]+=1
+        d1v=list(d1.values())
+        d2v=list(d2.values())
+        k1=list(d1.keys())
+        k2=list(d2.keys())
+        k1.sort()
+        k2.sort()
+        d1v.sort()
+        d2v.sort()
+        return d1v==d2v and k1==k2   
