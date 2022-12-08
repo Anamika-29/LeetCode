ebfb447e4f099dff3eb440e2bc0798bca20aa46c
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-08 17:52:30 +0530

    Time: 40 ms (82.21%), Space: 14 MB (9.82%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -1,34 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    void dfs(TreeNode* root, vector<int>& v) {
-        if (!root) return;
-        if (!(root->left || root->right)) {
-            v.push_back(root->val);
-        }
-        dfs(root->left, v);
-        dfs(root->right, v);
+class Solution:
+    def __init__(self):
+        self.n = []
         
-    }
-    
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> v1{};
-        vector<int> v2{};
+    def dfs(self, root):
+        if root:
+			# checking if the node is leaf
+            if not root.left and not root.right:  
+				# appends the leaf nodes to the list - self.n 
+                self.n.append(root.val)  
+				
+            self.dfs(root.left)
+            self.dfs(root.right)
+            
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        self.dfs(root1)
+        a = self.n
+        self.n = []
+        self.dfs(root2)
         
-        dfs(root1, v1);
-        dfs(root2, v2);
-        
-        return v1 == v2;
-        
-    }
-};
+        if a == self.n:
+            return True
+        else:
+            return False
