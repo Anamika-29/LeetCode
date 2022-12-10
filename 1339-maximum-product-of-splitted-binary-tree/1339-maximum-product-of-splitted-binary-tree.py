49e9cc9a0344545e82e1edcdb11b7cdc456765e3
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-10 11:00:53 +0530

    Time: 443 ms (78.38%), Space: 75.5 MB (45.15%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -1,23 +1,19 @@
-class Solution {
-public:
-    long totalSum = 0, maxPro = 0;
-    void cal_totalSum(TreeNode* root){
-        if(root == NULL) return;
-        cal_totalSum(root->left);
-        cal_totalSum(root->right);
-        totalSum += root->val;
-    }
-    long cal_maxPro(TreeNode* root){
-        if(root == NULL) return 0;
-        long l = cal_maxPro(root->left);
-        long r = cal_maxPro(root->right);
-        long SubtreeSum = l + r + root->val;
-        maxPro = max(maxPro, (totalSum - SubtreeSum)*SubtreeSum);
-        return SubtreeSum;
-    }
-    int maxProduct(TreeNode* root) {
-        cal_totalSum(root);
-        cal_maxPro(root);
-        return (int)(maxPro%1000000007);
-    }
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxProduct(self, root):
+        self.res = total = 0
+
+        def s(root):
+            if not root: return 0
+            left, right = s(root.left), s(root.right)
+            self.res = max(self.res, left * (total - left), right * (total - right))
+            return left + right + root.val
+
+        total = s(root)
+        s(root)
+        return self.res % (10**9 + 7)
