f5997aa9962b5ce0f1af205a037f4c5f1444401f
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-13 18:49:09 +0530

    Time: 17 ms (86.50%), Space: 9.9 MB (84.29%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,10 +1,20 @@
-class Solution:
-    def minFallingPathSum(self, A):
-        """
-        :type A: List[List[int]]
-        :rtype: int
-        """
-        dp = A[0]
-        for row in A[1:]:
-            dp = [value + min([dp[c], dp[max(c - 1, 0)], dp[min(len(A) - 1, c + 1)]]) for c, value in enumerate(row)]
-        return min(dp)   
+class Solution {
+public:
+   int minFallingPathSum(vector<vector<int>>& A) {
+        int row=A.size();
+        int column=A[0].size();
+        for(int i=1;i<row;i++){
+            for(int j=0;j<column;j++){
+                if(j==0)                                                    //Leftmost or first column
+                A[i][j]+=min(A[i-1][j],A[i-1][j+1]);
+                else if(j==column-1)A[i][j]+=min(A[i-1][j],A[i-1][j-1]);     //Rightmost or last column
+                else A[i][j]+=std::min({A[i-1][j],A[i-1][j+1],A[i-1][j-1]}); //Remaining cases
+            }
+        }
+        int sum=INT_MAX;
+        for(int i=0;i<column;i++){
+            sum=min(sum,A[row-1][i]);                                      //Last row scan
+        }
+        return sum;
+    }
+};
