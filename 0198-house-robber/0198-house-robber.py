0071aa1e0f6555d6cd2db950cff7875d3d63552f
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-14 20:10:49 +0530

    Time: 55 ms (59.77%), Space: 14 MB (20.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,22 @@
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-        vector<int>dp(nums.size());
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<nums.size();i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-        }
-        return dp[nums.size()-1];
-    }
-};
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums)
+        if length==0:
+            return 0
+        if length==1:
+            return nums[0]
+        if length==2:
+            return max(nums)
+			
+        dp = [0]*length # assign dp array
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        
+        for i in range(2, length):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        print(dp)
+        
+        return max(dp)
