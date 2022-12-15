660403a24ce6d0068a6aae65c4d6a362ccddbba8
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-15 13:01:55 +0530

    Time: 14 ms (91.31%), Space: 6.3 MB (99.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,10 @@
-from functools import lru_cache
-
-class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
-        @lru_cache(maxsize=None)
-        def memo_solve(ptr1, ptr2):
-            if ptr1 == len(text1) or ptr2 == len(text2):
-                return 0
-            
-            # Case 1
-            if text1[ptr1] == text2[ptr2]:
-                return 1 + memo_solve(ptr1+1, ptr2+1)
-        
-            # Case 2
-            else:     
-                return max(memo_solve(ptr1+1, ptr2), memo_solve(ptr1,ptr2+1))
-                       # ^    # ^ Case 2 - Option 1           ^ Case 2 - Option 2
-					   # | __You want the max() result from resulting branches in the tree 
-        return memo_solve(0,0) # Start the recursion stack from str1[0] and str2[0]	
+class Solution {
+public:
+   int longestCommonSubsequence(string &a, string &b) {
+    short m[2][1000] = {};
+    for (int i = 0; i < a.size(); ++i)
+        for (int j = 0; j < b.size(); ++j)
+            m[!(i % 2)][j + 1] = a[i] == b[j] ? m[i % 2][j] + 1 : max(m[i % 2][j + 1], m[!(i % 2)][j]);
+    return m[a.size() % 2][b.size()];
+}
+};
