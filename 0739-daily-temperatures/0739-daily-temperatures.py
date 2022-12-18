fb86b3dfd83873b9ad559952b2df76c1543a6984
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-18 13:16:38 +0530

    Time: 3530 ms (38.35%), Space: 30.5 MB (13.27%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,45 +1,15 @@
-class Solution {
-public:
-    vector<int> dailyTemperatures(vector<int>& T) {
-        if (T.empty())
-        {
-            return vector<int>();
-        }
-        
-		// A stack with values:  
-		// <T[i], Number of days to the next larger number in T>
-        stack<pair<int, int>> s;
-		
-		// The last number in T must have no value larger than it.
-        s.push(make_pair(T.back(), 0));
-        
-        vector<int> ans(T.size(), 0);
-        
-		// Start from the back.
-        for (int i = T.size() - 2; i >= 0; i--)
-        {
-            int counter = 1;
-            
-			// Pop the stack until its empty or the top number is smaller or equal to T[i]
-			// The counter + the number of elements to the next larger number in T.
-            while (!s.empty() && T[i] >= s.top().first)
-            {
-                counter += s.top().second;
-                s.pop();
-            }
-            
-			// If no number on stack is larger than T[i].
-            if (s.empty())
-            {
-                s.push(make_pair(T[i], 0));
-            }
-            else
-            {
-                s.push(make_pair(T[i], counter));
-                ans[i] = counter;
-            }
-        }
-        
-        return ans;
-    }
-    };
+class Solution:
+    def dailyTemperatures(self, T):
+    #initialize the result array with all '0's considering when there is no bigger temperature
+        ans = [0]*len(T) 
+        stack = []
+
+        for i,v in enumerate(T):
+            #check whether current val is greater than the last appended stack value.  We will pop all the elements which is lesser than the current temp
+            while stack and stack[-1][1] < v:
+                index,value = stack.pop()
+                ans[index] = i - index # we are checking how many indices we have crossed since we last have a lesser temperature
+            #Remember since we are comparing all the stack elements before inserting,all the stack elements will have temperatures greater than the current value	
+            stack.append([i,v])      
+
+        return ans   
