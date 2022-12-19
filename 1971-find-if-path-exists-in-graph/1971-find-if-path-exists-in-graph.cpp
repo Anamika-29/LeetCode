bc59d77455da5e60cb42ec7eb71a32d93ab9c045
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-19 19:57:21 +0530

    Time: 910 ms (72.71%), Space: 193.1 MB (38.18%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,32 +1,28 @@
-class Solution:
-    def validPath(self, n, edges, start, end):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :type start: int
-        :type end: int
-        :rtype: bool
-        """
-        visited = [False]*n
-        d = {}
-		#store the undirected edges for both vertices
-        for i in edges:
-            if i[0] in d:
-                d[i[0]].append(i[1])
-            else:
-                d[i[0]] = [i[1]]
-                
-            if i[1] in d:
-                d[i[1]].append(i[0])
-            else:
-                d[i[1]] = [i[0]]
-        #create a queue as we will apply BFS
-        q = [start]
-        while q:
-            curr = q.pop(0)  #pop the first element as we do in queue
-            if curr == end:  #if its the end then we can return True
-                return True
-            elif curr in d and not visited[curr]: #else if it is not the end then check whether its visited or not
-                q.extend(d[curr])  #add the adjacent vertices of the current node to the queue
-            visited[curr] = True  #mark this curr vertex as visited = True, so that we dont visit this vertex again
-        return False  #return False if the queue gets empty and we dont reach the end
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+        unordered_map<int,vector<int>> graph; 
+        for(auto e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+        vector<bool> visited(n,0);        
+        queue<int> q;
+        q.push(start);
+        visited[start] = 1; 
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            if(curr == end)
+                return 1; 
+            for(auto &node : graph[curr]){
+                if(!visited[node]){
+                    visited[node] = 1; 
+                    q.push(node);
+                }
+            }
+        }
+        
+        return false;
+    }
+};
