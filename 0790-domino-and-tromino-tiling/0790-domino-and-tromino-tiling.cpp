5c42809cfe1abb2f0dfd379733e175e27fc45bf3
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-24 18:30:58 +0530

    Time: 3 ms (55.84%), Space: 6.1 MB (71.49%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,6 +1,10 @@
-class Solution(object):
-    def numTilings(self, n):
-        dp = [1, 2, 5] + [0] * n
-        for i in range(3, n):
-            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % 1000000007
-        return dp[n - 1]
+class Solution {
+public:
+    int numTilings(int n) {
+    unsigned int dp[n + 3]; dp[0] = 1; dp[1] = 2; dp[2] = 5;
+    for (int i = 3; i < n; i ++) {
+        dp[i] = (2 * dp[i - 1] + dp[i - 3]) % 1000000007;
+    }
+    return dp[n - 1];
+    }
+};
