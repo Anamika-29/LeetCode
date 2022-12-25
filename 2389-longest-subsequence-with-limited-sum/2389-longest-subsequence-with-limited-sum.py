2aeb8b633ada16e50d0e87ed50f417a33496e1f4
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-25 13:53:28 +0530

    Time: 96 ms (98.93%), Space: 14.1 MB (98.93%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,12 +1,4 @@
-class Solution {
-public:
-    vector<int> answerQueries(vector<int> A, vector<int>& queries) {
-        sort(A.begin(), A.end());
-        vector<int> res;
-        for (int i = 1; i < A.size(); ++i)
-            A[i] += A[i - 1];
-        for (int& q: queries)
-            res.push_back(upper_bound(A.begin(), A.end(), q) - A.begin());
-        return res;
-    }
-};
+class Solution:
+    def answerQueries(self, A: List[int], queries: List[int]) -> List[int]:
+        A = list(accumulate(sorted(A)))
+        return [bisect_right(A, q) for q in queries]
