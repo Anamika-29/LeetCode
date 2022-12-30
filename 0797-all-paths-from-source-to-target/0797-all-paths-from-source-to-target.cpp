7d354cb6effd5f58b308fe7267295b3b9be4caaa
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-30 15:23:12 +0530

    Time: 18 ms (76.04%), Space: 12.1 MB (51.96%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,18 +1,23 @@
-class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        result = []
-        partialResult = []
-
-        def findPath(node):
-            if node == len(graph)-1:
-                result.append(partialResult.copy())
-                return
-
-            for i in graph[node]:
-                partialResult.append(i)
-                findPath(i)
-                partialResult.pop()
-
-        partialResult.append(0)
-        findPath(0)
-        return result
+class Solution {
+public:
+    int target;
+    vector<vector<int>> res;
+    vector<int> tmp;
+    void dfs(vector<vector<int>>& graph, int currNode = 0) {
+	    // updating tmp
+        tmp.push_back(currNode);
+		// and either updating res with it if target is met
+        if (currNode == target) res.push_back(tmp);
+		// or callling dfs again recursively
+        else for (int node: graph[currNode]) {
+            dfs(graph, node);
+        }
+        // backtracking with tmp
+		tmp.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        target = graph.size() - 1;
+        dfs(graph);
+        return res;
+    }
+};
