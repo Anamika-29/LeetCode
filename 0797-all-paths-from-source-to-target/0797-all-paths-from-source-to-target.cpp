5d15dcab7debd5f496cbd976023939e5cf6d26ed
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2022-12-30 15:22:56 +0530

    Time: 101 ms (90.38%), Space: 15.7 MB (43.50%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,18 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        result = []
+        partialResult = []
+
+        def findPath(node):
+            if node == len(graph)-1:
+                result.append(partialResult.copy())
+                return
+
+            for i in graph[node]:
+                partialResult.append(i)
+                findPath(i)
+                partialResult.pop()
+
+        partialResult.append(0)
+        findPath(0)
+        return result
