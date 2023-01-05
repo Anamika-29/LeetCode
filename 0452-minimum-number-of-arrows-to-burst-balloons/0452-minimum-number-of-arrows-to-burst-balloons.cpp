ad1382dbe9708fa21f7847567e40c11ecbccfa4a
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-05 21:08:49 +0530

    Time: 1097 ms (23.39%), Space: 89.8 MB (28.76%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,25 +1,16 @@
-class Solution:
-    def findMinArrowShots(self, points):
-        n = len(points)
-        if n<2:
-            return n
 
-        #sort by start and end point
-        START, END = 0,1
-        points.sort(key=lambda i: (i[START],i[END]) )
-        prev, cur = points[0], None
-        darts = 0
-
-        for i in range(1, n):
-            cur = points[i]
-
-            if cur[START] <= prev[END]:
-                #overlap, wait for more overlap to throw dart
-                prev = [cur[START], min(cur[END],prev[END])]
-            else:
-                #no overlap, throw dart at previous
-                darts += 1
-                prev = cur
-		
-		#pop the last balloon and return
-        return darts+1
+    bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
+class Solution {
+public:  
+    int findMinArrowShots(vector<vector<int>>& segments) {
+        sort(segments.begin(), segments.end(), cmp);
+        int ans = 0, arrow = 0;
+        for (int i = 0; i < segments.size(); i ++) {
+            if (ans == 0 || segments[i][0] > arrow) {
+                ans ++;
+                arrow = segments[i][1];
+            }
+        }
+        return ans;
+    }
+};
