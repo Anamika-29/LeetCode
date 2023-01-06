0de95bb180da87641ed17610f4f9a2ba4f2fd001
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-06 21:04:53 +0530

    Time: 1463 ms (45.29%), Space: 28.1 MB (17.54%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,15 +1,8 @@
-class Solution {
-public:
-    int maxIceCream(vector<int>& costs, int coins) {
-    sort(costs.begin(),costs.end());
-    int n=costs.size();
-    for (int i = 0; i <n;i++)
-    //we will check we can buy ice bar or not
-        if (coins >= costs[i])
-            coins -= costs[i];//update the coins value
-        else
-            return i;//we can't purchase anymore so will return ans
-    return n;
-       }
-
-};
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        cn=0
+        for i in sorted(costs):
+            if i<=coins:
+                cn+=1
+                coins-=i
+        return cn
