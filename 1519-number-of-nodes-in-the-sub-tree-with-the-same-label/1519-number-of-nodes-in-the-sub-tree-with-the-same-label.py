0c5b4742940c9e5c163e78577e8f15b12b3f7937
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-12 19:51:10 +0530

    Time: 3036 ms (74.36%), Space: 191.5 MB (51.28%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -1,30 +1,19 @@
-class Solution {
-public:
-  vector<int> fun(vector<vector<int>> &adj, string &labels, int i,vector<int>&result){
-    vector<int> ans(26, 0);
-    result[i] = 1;
-    ans[labels[i] - 'a'] = 1;
-    
-    for(int j = 0; j != adj[i].size(); j++)
-      if(!result[adj[i][j]]){
-        vector<int> tmp = fun(adj, labels,adj[i][j],result);
-        for(int k = 0; k != 26; k++) ans[k] += tmp[k];
-      }
-    
-    result[i] = ans[labels[i] - 'a'];
-    
-    return ans;
-  }
-  
-  vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-    vector<vector<int>> adj(n);
-    vector<int> result(n,0);
-    for(int i = 0; i != edges.size(); i++)
-     {adj[edges[i][0]].push_back(edges[i][1]);
-      adj[edges[i][1]].push_back(edges[i][0]);
-     }
-    
-    fun(adj, labels, 0,result);
-    return result;
-  }
-};
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+        ans = [0] * n
+        tree = collections.defaultdict(list)
+        for a, b in edges:                             # build tree
+            tree[a].append(b)
+            tree[b].append(a)
+        def dfs(node):                                 # dfs
+            nonlocal visited, ans, tree
+            c = collections.Counter(labels[node])
+            for nei in tree[node]:
+                if nei in visited: continue            # avoid revisit
+                visited.add(nei)
+                c += dfs(nei)                          # add counter (essentially adding a 26 elements dictionary)
+            ans[node] = c.get(labels[node])            # assign count of label to this node
+            return c
+        visited = set([0])
+        dfs(0)
+        return ans
