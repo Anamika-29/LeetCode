b08827233538cd2b49644dd3e1076a219d51fb70
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-13 21:07:27 +0530

    Time: 435 ms (81.17%), Space: 181.3 MB (65.77%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,23 +1,23 @@
-class Solution:
-    def longestPath(self, parent: List[int], s: str) -> int:
-        t={}
-        for i in range(1,len(parent)):
-            if parent[i] not in t:
-                t[parent[i]]=[i]
-            else:
-                t[parent[i]].append(i)
-            
-        self.ans=1
-        def fun(i):
-            if i not in t:
-                return 1
-            res = 1
-            for j in t[i]:
-                length=fun(j)
-                if s[i] != s[j]:
-                    self.ans = max(self.ans,length+res)
-                    res = max(res,length+1)
-            return res
-        
-        fun(0)
-        return self.ans
+class Solution {
+public:
+    int longestPath(vector<int>& parent, string s) {
+        int n = s.size(), res = 0;
+        vector<vector<int>> children(n, vector<int>());
+        for (int i = 1; i < n; ++i)
+            children[parent[i]].push_back(i);
+        dfs(children, s, res, 0);
+        return res;
+    }
+
+    int dfs(vector<vector<int>>& children, string& s, int& res, int i) {
+        int big1 = 0, big2 = 0;
+        for (int& j : children[i]) {
+            int cur = dfs(children, s, res, j);
+            if (s[i] == s[j]) continue;
+            if (cur > big2) big2 = cur;
+            if (big2 > big1) swap(big1, big2);
+        }
+        res = max(res, big1 + big2 + 1);
+        return big1 + 1;
+    }
+};
