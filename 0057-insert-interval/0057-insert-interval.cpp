371f786481d709741683e483dd412d373bbdda1b
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-16 20:03:26 +0530

    Time: 11 ms (97.45%), Space: 17.1 MB (78.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,15 +1,23 @@
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        heap, ans, = [], [] 
-        for s, e in intervals + [newInterval]: # add start & end to heap (-1 is start, 1 is end)
-            heapq.heappush(heap, (s, -1))
-            heapq.heappush(heap, (e, 1))
-        cur, s = 0, None            
-        while heap:                            
-            i, val = heapq.heappop(heap)       # pop heap
-            if s is None: s = i                # is s is None, assign i to s (interval start)
-            cur += val                         # keep counting until close interval
-            if not cur:                        # when cur == 0, meaning we can close the interval
-                ans.append([s, i])             # append interval to ans
-                s = None                       # reset s to None
-        return ans 
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+         vector<vector<int>>ans;
+        for(auto &&i:intervals){
+    //  the new interval is after the range of other interval
+            if(i[1]<newInterval[0])ans.push_back(i);
+    // the new interval's range is before the other
+            else if(newInterval[1]<i[0]){
+                ans.push_back(newInterval);
+                newInterval=i;//updating the new interval
+            }
+    // the new interval is in the range of the other interval
+            else{
+                newInterval[0]=min(newInterval[0],i[0]);
+                newInterval[1]=max(newInterval[1],i[1]);
+            }    
+        }
+    //At the end after the loop just add the updated newIntervals
+        ans.push_back(newInterval);
+        return ans;
+    }
+};
