943a6caae45c8d9db6e49bb044d10536861ea1db
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-23 10:24:34 +0530

    Time: 772 ms (67.07%), Space: 19 MB (59.07%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,12 +1,10 @@
-class Solution {
-public:
-      int findJudge(int N, vector<vector<int>>& trust) {
-        vector<int> count(N + 1, 0);
-        for (auto& t : trust)
-            count[t[0]]--, count[t[1]]++;
-        for (int i = 1; i <= N; ++i) {
-            if (count[i] == N - 1) return i;
-        }
-        return -1;
-    }
-};
+class Solution:
+     def findJudge(self, N, trust):
+        count = [0] * (N + 1)
+        for i, j in trust:
+            count[i] -= 1
+            count[j] += 1
+        for i in range(1, N + 1):
+            if count[i] == N - 1:
+                return i
+        return -1
