431ea2ea6a67bad543e928eb1d776d1c699832c0
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-26 17:57:59 +0530

    Time: 113 ms (77.20%), Space: 15.4 MB (26.71%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,41 +1,21 @@
-class Solution {
-public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> graph(n);
-        for(auto e : flights) {
-            graph[e[0]].push_back({e[1], e[2]});
-        }
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        // {dist_from_src_node, node, number_of_stops_from_src_node}
-        pq.push({0, src, 0});
-        
-        vector<int> stops(n, INT_MAX);
-        // number of stops to reach indexth node with least possible price from src node
-        // as it will be calculated once the pq.top() equals to indexth node
-        
-        // pq.top() will always store least cost among the pq elements so if already stop is calculated
-        // and if that is greater than the cstop that means already we have a path with cheaper cost
-        // as well as with less or equal number of stops
-        
-        while(!pq.empty()) {
-            auto temp=pq.top();
-            int cdist=temp[0];
-            int cnode=temp[1];
-            int cstop=temp[2];
-            pq.pop();
-            
-            if(cstop>stops[cnode] || cstop>k+1)
-                continue;
-            
-            stops[cnode]=cstop;
-            if(cnode==dst) {
-                return cdist;
-            }
-            
-            for(auto a : graph[cnode]) {
-                pq.push({cdist+a.second, a.first, cstop+1});
-            }
-        }
-        return -1;
-    }
-};
+class Solution:
+  def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    graph = [[] for _ in range(n)]
+    minHeap = [(0, src, k + 1)]  # (d, u, stops)
+    dist = [[math.inf] * (k + 2) for _ in range(n)]
+
+    for u, v, w in flights:
+      graph[u].append((v, w))
+
+    while minHeap:
+      d, u, stops = heapq.heappop(minHeap)
+      if u == dst:
+        return d
+      if stops > 0:
+        for v, w in graph[u]:
+          newDist = d + w
+          if newDist < dist[v][stops - 1]:
+            dist[v][stops - 1] = newDist
+            heapq.heappush(minHeap, (newDist, v, stops - 1))
+
+    return -1
