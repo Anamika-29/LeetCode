e9820029bc6246f5bacaadbf4090acb6272ae352
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-01-27 20:11:47 +0530

    Time: 267 ms (65.74%), Space: 48.4 MB (61.45%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,40 +1,27 @@
-class TrieNode:
-    def __init__(self):
-        self.children = [None] * 26
-        self.is_end = False
-
-class Solution:
-    def __init__(self):
-        self.root = TrieNode()
-        
-    def insert(self, root, key):
-        curr = root
-        for i in range(len(key)):
-            idx = ord(key[i]) - ord('a')
-            if not curr.children[idx]:
-                curr.children[idx] = TrieNode()
-            curr = curr.children[idx]
-        curr.is_end = True
-
-    def dfs(self, root, key, index, count):
-        if index >= len(key):
-            return count > 1
-        curr = root
-        for i in range(index, len(key)):
-            p = ord(key[i]) - ord('a')
-            if not curr.children[p]:
-                return False
-            curr = curr.children[p]
-            if curr.is_end:
-                if self.dfs(root, key, i+1, count+1):
-                    return True
-        return False
-
-    def findAllConcatenatedWordsInADict(self, words):
-        for i in range(len(words)):
-            self.insert(self.root, words[i])
-        ans = []
-        for i in range(len(words)):
-            if self.dfs(self.root, words[i], 0, 0):
-                ans.append(words[i])
-        return ans
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> words_set;
+        for (string word : words) words_set.insert(word);
+        vector<string> res;
+    
+        for (string word : words) {
+            int n = word.size();
+            vector<int> dp(n + 1, 0);
+            dp[0] = 1;
+            for (int i = 0; i < n; i++) {
+                if (!dp[i]) continue;
+                for (int j = i + 1; j <= n; j++) {
+                    if (j - i < n && words_set.count(word.substr(i, j - i))) {
+                        dp[j] = 1;
+                    }
+                }
+                if (dp[n]) {
+                    res.push_back(word);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
